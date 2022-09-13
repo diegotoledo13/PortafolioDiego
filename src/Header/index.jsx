f88433fc32b00132a7d0dff29c24d277208cc215
Diego <105314333+diegokill13@users.noreply.github.com>
2022-09-13 19:17:16 -0300

    Maquetando parte 1 13.09.22

---
+++ src/Header/index.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import './header.css';
+
+
+function Header () {
+    return(
+        <div>
+            <header className="main-header">
+            <h1 className="my-name">Diego Toledo</h1>
+            
+            </header>
+        </div>
+    );
+    }
+
+export { Header };
