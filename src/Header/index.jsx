68a98dbc7a409da7e3ca7d182c6600a594e3e305
Diego <105314333+diegokill13@users.noreply.github.com>
2022-09-15 21:36:15 -0300

    encontrando el bug

--- src/Header/index.jsx
+++ src/Header/index.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import './header.css';
-import instagram from '../public/icons/instagram.png';
-import linkedin from '../public/icons/linkedin.png';
-import github from '../public/icons/github.png';
-import gmail from '../public/icons/gmail.png';
+import instagram from '../assets/icons/instagram.png';
+import linkedin from '../assets/icons/linkedin.png';
+import github from '../assets/icons/github.png';
+import gmail from '../assets/icons/gmail.png';
 
 function Header () {
     return(
         <div>
             <header className="main-header">
                 <h1 className="main-header-name">Diego Toledo</h1>
-                <img className="main-header-img" src="/src/public/images/imgDiego.png" alt="" />
+                <img className="main-header-img" src="/src/assets/images/imgDiego.png" alt="Imagen Diego" />
                 <div class="social-container">
                     <ul>
                         <a href="#"><img src={instagram} alt="Instagram" /> </a>
