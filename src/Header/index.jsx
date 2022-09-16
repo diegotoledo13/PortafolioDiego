eaf62abf52ca709081779f31abeb9a440da254ea
Diego <105314333+diegokill13@users.noreply.github.com>
2022-09-15 21:16:28 -0300

    ajustando header de la web

--- src/Header/index.jsx
+++ src/Header/index.jsx
@@ -1,16 +1,28 @@
 import React from "react";
 import './header.css';
-
+import instagram from '../public/icons/instagram.png';
+import linkedin from '../public/icons/linkedin.png';
+import github from '../public/icons/github.png';
+import gmail from '../public/icons/gmail.png';
 
 function Header () {
     return(
         <div>
             <header className="main-header">
-            <h1 className="my-name">Diego Toledo</h1>
-            
+                <h1 className="main-header-name">Diego Toledo</h1>
+                <img className="main-header-img" src="/public/images/imgDiego.png" alt="" />
+                <div class="social-container">
+                    <ul>
+                        <a href="#"><img src={instagram} alt="Instagram" /> </a>
+                        <a href="#"><img src={linkedin} alt="linkedin" /></a>
+                        <a href="#"><img src={github} alt="github" /></a>
+                        <a href="#"><img src={gmail} alt="gmail" /></a>
+                    </ul>
+                </div>
             </header>
         </div>
     );
     }
+    
 
 export { Header };
