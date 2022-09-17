32fecf9b9dcaea1fc5acef2273af6d66e3cb75c4
Diego <105314333+diegokill13@users.noreply.github.com>
2022-09-17 18:44:34 -0300

    agregando el texto de contactos

--- src/Section/index.jsx
+++ src/Section/index.jsx
@@ -12,23 +12,29 @@ function Section() {
         ejecutar soluciones. Disfruto de los desafíos y como autodidacta siempre
         me encuentro en un continuo proceso de aprendizaje.
       </h3>
-      <section>
-        <p className="phone"></p>
+      <div className="main-contact">
+        <section>
+          <p className="phone">+59899818265</p>
 
-        <img src={images.call} alt="Phone" />
-      </section>
-      <section>
-        <p className="mail"></p>
-        <img src={images.mail} alt="Mail" />
-      </section>
-      <section>
-        <p className="home"></p>
-        <img src={images.address} alt="Address" />
-      </section>
-      <section>
-        <p className="skills"></p>
-        <img src={images.skills} alt="Skills" />
-      </section>
+          <img src={images.call} alt="Phone" />
+        </section>
+        <section className="mail">
+          <p>diegomasteruno13@gmail.com</p>
+          <img src={images.mail} alt="Mail" />
+        </section>
+        <section className="home">
+          <p>Maldonado - Uruguay</p>
+          <img src={images.address} alt="Address" />
+        </section>
+        <section className="skills">
+          <li>Front-end Developer</li>
+          <li>Diseñador Gráfico</li>
+          <li>
+            Técnico en Reparación y <br></br> Mantenimiento de PC y Redes
+          </li>
+          <img src={images.skills} alt="Skills" />
+        </section>
+      </div>
     </div>
   );
 }
